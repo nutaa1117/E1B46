422973e6a537d43d2a82a06f9beda3aa51d5ece0
施家誠 <u11202246@o365.ttu.edu.tw>
2024-05-05 23:47:37 +0800

    完成 把註解給完成。 心得: 這次用github來繳交作業，是完全沒有用過這個東西的， 不過看起來是一個非常重要的工具，會使用的話對於未來找工作可能會有 不錯的加分，現在也只會一些很基本的指令，往後如果有機會再學會更多關於 git指令的寫法。而這次的作業，練習了很多迴圈的寫法，用for迴圈去印出像是直角三角形、 乘法表等等，可以有效的訓練寫巢狀迴圈的能力，也再一次的練習 switch的寫法，上一次的作業也有練習到，只是期中考的時候突然忘記怎麼 用，一些順序以及符號打錯，導致一直無法編譯，這次作業再練習一次，可以更 加深我對switch的寫法，還有熟練度。

--- HW2.c
+++ HW2.c
@@ -35,9 +35,9 @@ int main(void)
     while (input != 2024)
     {                 //while判斷是不是2024
         n++;
-        if (n == 3)
+        if (n == 3)                 //用if來判斷n，可知道輸入了幾次 
         {
-            printf("輸錯三次");
+            printf("輸錯三次");        //印出輸錯三次 
             return 0;
         }
         printf("密碼錯誤");
@@ -59,33 +59,33 @@ int main(void)
     system("pause");
     system("cls");
 
-    switch (choose)
+    switch (choose)            //switch來判斷使用者輸入了甚麼 
     {
 
-    case 'a':
+    case 'a':                         //a或A的話 
     case 'A':
-        printf("請輸入一個字元（a-n）：");
+        printf("請輸入一個字元（a-n）：");          //要求輸入字元a~n 
         scanf(" %c", &draw);
 
-        if (draw < 'a' || draw > 'n')
+        if (draw < 'a' || draw > 'n')              //判斷輸入的字元有沒有在範圍裡
         {
-            printf("請輸入有效範圍的字元（a-n）\n");  //判斷輸入的字元有沒有在範圍裡
+            printf("請輸入有效範圍的字元（a-n）\n");  
             scanf(" %c", &draw);
         }
 
-        char c, d;
-        for (c = 'a'; c <= draw; c++) {
-            for (d = draw; d > c; d--) {
+        char c, d;                              
+        for (c = 'a'; c <= draw; c++) {               //從a開始直到輸入的字元 
+            for (d = draw; d > c; d--) {           //內部迴圈，從輸入的字元開始，直到與外部迴圈相同 
                 printf("  "); // 印出空格讓直角三角形靠右
             }
-            for (d = 'a'; d <= c; d++) {
+            for (d = 'a'; d <= c; d++) {            //內部迴圈從a開始直到與外部相同 
                 printf("%c ", d); // 印出字元
             }
             printf("\n"); 
         }
         break;
     
-		case 'b':
+		case 'b':                 //輸入b或B時 
         case 'B':
                 
                 printf("請輸入一個整數(1-9)：");  
@@ -96,24 +96,24 @@ int main(void)
                 }
 
                 
-                int i=0,j=0;
-                for (i=1; i<=v;i++) {
-                    for (j=1; j<=v; j++) 
+                int i=0,j=0;                   
+                for (i=1; i<=v;i++) {             //印出乘法表 
+                    for (j=1; j<=v; j++)           
                         printf("%4d\n",i*j);
                     	printf("\n");
                 }break;
                 
-          	case 'c':
+          	case 'c':                //如果輸入的字元是c或C的話 
     		case 'C':
-       		 printf("continue(y/n)?");
+       		 printf("continue(y/n)?");             //印出是否繼續 
         	char a;
-        	scanf(" %c",&a);
-        	while(a!='y'&&a!='Y'&&a!='n'&&a!='N')   {
+        	scanf(" %c",&a);                   //要求使用者輸入字元 
+        	while(a!='y'&&a!='Y'&&a!='n'&&a!='N')   {            //如果輸入的字元不是y,Y,n,N,就會要求重輸 
             	 printf("continue(y/n)?");
             	 scanf(" %c",&a);
 			} 
-			if(a=='y' || a=='Y'){
-        	printf("--------------------------\n");
+			if(a=='y' || a=='Y'){                            //如果輸入y或Y 
+        	printf("--------------------------\n");              //重新回到二，執行程式 
     printf("|  a. 畫出直角三角形      |\n");          //製作選單
     printf("|  b. 顯示乘法表          |\n");
     printf("|  c. 結束                |\n");
@@ -179,31 +179,12 @@ int main(void)
 			} 
 			if(a=='y' || a=='Y'){
         	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 			}      
             else if(a=='n'||a=='N'){
             	return 0;
 			}   
             
         
-	
-
-	
-
-	
-
 	
 	
 	
@@ -213,38 +194,15 @@ int main(void)
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 			}      
-            else if(a=='n'||a=='N'){
+            else if(a=='n'||a=='N'){          //如果輸入n或N的話，則程式結束 
             	return 0;
 			}   
             
         
 	
-
-	
-
-	
-
-	
-	
 	
 	}
-
-    
-
-
-
-
-
 
 
 }
