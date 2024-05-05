c137f1ed05d65ecb09509a58353a710d7b4c3c5a
施家誠 <u11202246@o365.ttu.edu.tw>
2024-05-05 21:28:27 +0800

    完成第三題，但是有錯誤 我把整個程式稍微對其整理，較好觀看，然後把第三題的程式打完， 不過有一個錯誤就是，如果輸錯範圍，就是a~n的範圍的話，我的程式會結束，因為用成break了， 要把它改成繼續輸入

--- HW2.c
+++ HW2.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(void)
 {
-	printf("  *****         \n");       //製作個人的畫面風格 
+    printf("  *****         \n");       //製作個人的畫面風格
     printf(" *     *        \n");
     printf("*  O O  *      \n");
     printf("*   ^   *     \n");
@@ -13,64 +14,79 @@ int main(void)
     printf(" |          |     **         |       *  *\n");
     printf(" -------  __|__   *  *       |       ****\n");
     printf("                  ***                \n");
-    
-    
+
+
     printf("    \\     /             \\             \\         \\    \\     \n");
-    printf("    ---  /-----      ---------       -----  -----------   \n");  
-	printf("     /               \\  ---- /        ---   |     \\     \n");
-	printf("    /--  -|-|--          /\\           ---   |---   \\  / \n");
- 	printf("   /  /   | | /           /\\          ---   |  |    \\/ \n");
-printf("  / \\/    |              /  \\        |   |  |\\ |    /\\ \n");
- printf("          -----              \\        ---   |         \\/  \n");
- printf("                           \\\\               |           \n");
-  system("pause");
-  system("cls");
-  
-  int password=2024,input=0,n=0;    //設定整數名稱 
-  	char choose;     //設定字元名稱 
-	char draw;
-  
-  
-  printf("請輸入密碼:\n");
-  scanf("%d",&input);
-  while(input!=2024){                 //while判斷是不是2024 
-		n++;
-		if(n==3){
-  		printf("輸錯三次");
-  		return 0;
-  		}
-  		printf("密碼錯誤");
-  		printf("再次輸入密碼:");
-  		scanf("%d",&input);
-  	
-	  
-  }
- printf("恭喜輸入正確"); 
- system("pause");
-  system("cls");
-  
-  printf("--------------------------\n");
-  printf("|  a. 畫出直角三角形      |\n");          //製作選單 
-  printf("|  b. 顯示乘法表          |\n");
-  printf("|  c. 結束                |\n");
-  printf("--------------------------- \n");
-        
-		printf("請選擇操作：");
-        scanf(" %c", &choose);
- system("pause");
-  system("cls");
-  
-  switch (choose) {                         
-  
-            case 'a':
-            case 'A':
-				 printf("請輸入一個字元（a-n）：");
-                scanf(" %c", &draw);
-                
-                if (draw<'a' || draw >'n') {
-                    printf("請輸入有效範圍的字元（a-n）\n");  //判斷輸入的字元有沒有在範圍裡 
-                    break;
-                }
- 
- 	}
- } 
+    printf("    ---  /-----      ---------       -----  -----------   \n");
+    printf("     /               \\  ---- /        ---   |     \\     \n");
+    printf("    /--  -|-|--          /\\           ---   |---   \\  / \n");
+    printf("   /  /   | | /           /\\          ---   |  |    \\/ \n");
+    printf("  / \\/    |              /  \\        |   |  |\\ |    /\\ \n");
+    printf("          -----              \\        ---   |         \\/  \n");
+    system("pause");
+    system("cls");
+
+    int password = 2024, input = 0, n = 0;    //設定整數名稱
+    char choose;
+    char draw;
+
+    printf("請輸入密碼:\n");
+    scanf("%d", &input);
+    while (input != 2024)
+    {                 //while判斷是不是2024
+        n++;
+        if (n == 3)
+        {
+            printf("輸錯三次");
+            return 0;
+        }
+        printf("密碼錯誤");
+        printf("再次輸入密碼:");
+        scanf("%d", &input);
+    }
+    printf("恭喜輸入正確");
+    system("pause");
+    system("cls");
+
+    printf("--------------------------\n");
+    printf("|  a. 畫出直角三角形      |\n");          //製作選單
+    printf("|  b. 顯示乘法表          |\n");
+    printf("|  c. 結束                |\n");
+    printf("--------------------------- \n");
+
+    printf("請選擇操作：");
+    scanf(" %c", &choose);
+    system("pause");
+    system("cls");
+
+    switch (choose)
+    {
+
+    case 'a':
+    case 'A':
+        printf("請輸入一個字元（a-n）：");
+        scanf(" %c", &draw);
+
+        if (draw < 'a' || draw > 'n')
+        {
+            printf("請輸入有效範圍的字元（a-n）\n");  //判斷輸入的字元有沒有在範圍裡
+            break;
+        }
+
+        char c, d;
+        for (c = 'a'; c <= draw; c++) {
+            for (d = draw; d > c; d--) {
+                printf("  "); // 印出空格讓直角三角形靠右
+            }
+            for (d = 'a'; d <= c; d++) {
+                printf("%c ", d); // 印出字元
+            }
+            printf("\n"); 
+        }
+        break;
+    }
+
+    system("pause");
+    return 0;
+}
+
