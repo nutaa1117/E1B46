7e152033de6be537535b50ed0fe568ed45edab63
施家誠 <u11202246@o365.ttu.edu.tw>
2024-05-05 17:43:50 +0800

    繼續製作個人封面 加上E1B46，還有未做完的名字

---
+++ HW2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	printf("  *****         \n");
+    printf(" *     *        \n");
+    printf("*  O O  *      \n");
+    printf("*   ^   *     \n");
+    printf(" * \\_/ *       \n");
+    printf(" -------   /|     ***       /|       ****\n");
+    printf(" |        / |     *  *     / |       *\n");
+    printf(" |----      |     * *     /__|_      ****\n");
+    printf(" |          |     **         |       *  *\n");
+    printf(" -------  __|__   *  *       |       ****\n");
+    printf("                  ***                \n");
+    
+    printf("\\                    \\             \\     \\    \\\n");
+    printf(" \\     /              \\                             \n");
+    printf(" ----  /-----       ---------         \n");    
+ } 
