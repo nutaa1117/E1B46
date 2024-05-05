b5be68c436c20abb623b83d9d83f18bd195f0586
施家誠 <u11202246@o365.ttu.edu.tw>
2024-05-05 23:26:00 +0800

    最後的switch c 快要完成了，只差最後把輸入y時回到2的地方補完了，要把上面的東西全部拉到 if裡面。

--- HW2.c
+++ HW2.c
@@ -103,9 +103,130 @@ int main(void)
                     	printf("\n");
                 }break;
                 
+          	case 'c':
+    		case 'C':
+       		 printf("continue(y/n)?");
+        	char a;
+        	scanf(" %c",&a);
+        	while(a!='y'&&a!='Y'&&a!='n'&&a!='N')   {
+            	 printf("continue(y/n)?");
+            	 scanf(" %c",&a);
+			} 
+			if(a=='y' || a=='Y'){
+        	printf("--------------------------\n");
+    printf("|  a. 畫出直角三角形      |\n");          //製作選單
+    printf("|  b. 顯示乘法表          |\n");
+    printf("|  c. 結束                |\n");
+    printf("--------------------------- \n");
+
+    printf("請選擇操作：");
+    scanf(" %c", &choose);
+    system("pause");
+    system("cls");
+
+    switch (choose)
+    {
+
+    case 'a':
+    case 'A':
+        printf("請輸入一個字元（a-n）：");
+        scanf(" %c", &draw);
+
+        if (draw < 'a' || draw > 'n')
+        {
+            printf("請輸入有效範圍的字元（a-n）\n");  //判斷輸入的字元有沒有在範圍裡
+            scanf(" %c", &draw);
+        }
+
+        char c, d;
+        for (c = 'a'; c <= draw; c++) {
+            for (d = draw; d > c; d--) {
+                printf("  "); // 印出空格讓直角三角形靠右
+            }
+            for (d = 'a'; d <= c; d++) {
+                printf("%c ", d); // 印出字元
+            }
+            printf("\n"); 
+        }
+        break;
+    
+		case 'b':
+        case 'B':
                 
-               
+                printf("請輸入一個整數(1-9)：");  
+                scanf("%d", &v);
+                if (v<1||v>9) {      //判斷是否在1~9的區間 
+                    printf("請輸入有效範圍的整數（1-9）\n");
+                    scanf("%d", &v);
+                }
+
                 
+                int i=0,j=0;
+                for (i=1; i<=v;i++) {
+                    for (j=1; j<=v; j++) 
+                        printf("%4d\n",i*j);
+                    	printf("\n");
+                }break;
+                
+          	case 'c':
+    		case 'C':
+       		 printf("continue(y/n)?");
+        	char a;
+        	scanf(" %c",&a);
+        	while(a!='y'&&a!='Y'&&a!='n'&&a!='N')   {
+            	 printf("continue(y/n)?");
+            	 scanf(" %c",&a);
+			} 
+			if(a=='y' || a=='Y'){
+        	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			}      
+            else if(a=='n'||a=='N'){
+            	return 0;
+			}   
+            
+        
+	
+
+	
+
+	
+
+	
+	
+	
+	}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			}      
+            else if(a=='n'||a=='N'){
+            	return 0;
+			}   
+            
         
 	
 
