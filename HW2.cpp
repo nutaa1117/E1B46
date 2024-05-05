00d037d001fa525f13e39862ffb29c9fa102cdcc
施家誠 <u11202246@o365.ttu.edu.tw>
2024-05-05 16:46:52 +0800

    第一次做編譯 做了一個笑臉

---
+++ HW2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	printf("  *****  \n");
+    printf(" *     * \n");
+    printf("*  O O  *\n");
+    printf("*   ^   *\n");
+    printf(" *\\_/ *\n");
+    printf("  *****  \n");
+
+ } 
