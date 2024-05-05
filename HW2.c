a7aed447baf938bb7cef553fc2b0a92bd25cb660
施家誠 <u11202246@o365.ttu.edu.tw>
2024-05-05 19:18:16 +0800

    完成了個人風格圖，開始做密碼 利用while來判斷是否輸對密碼，但是我編譯後的語意錯誤，因為if那邊打錯了，應該是n==3，我變成把n設定成3。

--- HW2.c
+++ HW2.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main(void)
 {
-	printf("  *****         \n");
+	printf("  *****         \n");       //製作個人的畫面風格 
     printf(" *     *        \n");
     printf("*  O O  *      \n");
     printf("*   ^   *     \n");
@@ -14,7 +14,36 @@ int main(void)
     printf(" -------  __|__   *  *       |       ****\n");
     printf("                  ***                \n");
     
-    printf("\\                    \\             \\     \\    \\\n");
-    printf(" \\     /              \\                             \n");
-    printf(" ----  /-----       ---------         \n");    
+    
+    printf("    \\     /             \\             \\         \\    \\     \n");
+    printf("    ---  /-----      ---------       -----  -----------   \n");  
+	printf("     /               \\  ---- /        ---   |     \\     \n");
+	printf("    /--  -|-|--          /\\           ---   |---   \\  / \n");
+ 	printf("   /  /   | | /           /\\          ---   |  |    \\/ \n");
+printf("  / \\/    |              /  \\        |   |  |\\ |    /\\ \n");
+ printf("          -----              \\        ---   |         \\/  \n");
+ printf("                           \\\\               |           \n");
+  system("pause");
+  system("cls");
+  
+  int password=2024,input=0,n=0;
+  
+  
+  
+  printf("請輸入密碼:\n");
+  scanf("%d",&input);
+  while(input!=2024){
+  		n++;
+		printf("密碼錯誤");
+  		printf("再次輸入密碼:");
+  		scanf("%d",&input);
+  	
+  	if(n=3){
+  		printf("輸錯三次");
+  		break;
+	  }
+  }
+ printf("恭喜輸入正確"); 
+ 
+ 
  } 
