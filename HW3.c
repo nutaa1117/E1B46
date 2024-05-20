c61e33cae1d66c2803f31fab570aa5eff4ddf97e
施家誠 <u11202246@o365.ttu.edu.tw>
2024-05-20 21:36:07 +0800

    完成!(心得) 加入了註解。 心得: 這次的作業很有挑戰，程式碼非常多，而且是一個 感覺真的有用的東西，就像是真的外面電影購票會 有的選位系統，雖然沒有到那麼逼真，但是這個程 式真的能達到基本的要求，想要電腦選位也可以， 還可以選擇要幾個位置，要自己選位也可以，然後 在寫這種程式的時候，函數真的變的很好用，因為 要不斷的回到主選單，所以先定義好以後，每次就 只要叫出函數就可以，整個程式的可讀性也更高了 ，畫面會相對乾淨很多，在case裡面也不會很亂， 需要執行的函數都統一放在主程式外了，在寫這種 比較多、比較雜的程式時，才知道為甚麼一定要用 函數，不然我都是直接寫在裡面，看起來就會很亂 ，重複的東西，還會一直重新編譯，導致速度變慢。  Please enter the commit message for your changes. Lines starting

--- HW3.c
+++ HW3.c
@@ -54,37 +54,37 @@ void displayseats(char seats[ROWS][COLS]){
 }
    
    void suggestSeats(char seats[ROWS][COLS], int numSeats){  
-    int found = 0;
+    int found = 0;                 //標記是否找到合適的座位
     int row, col, i, fit;
-    while(!found){
+    while(!found){                    //當還沒找到合適的座位時 
     	row=rand()%ROWS;
-    	col=rand()%(COLS-numSeats+1);
+    	col=rand()%(COLS-numSeats+1);          //隨機生成列，確保可以容納numSeats個連續座位 
     	fit=1;
 	  
-	  for(i=0;i<numSeats;i++){
-	  	if(seats[row][col+i]!='-'){
+	  for(i=0;i<numSeats;i++){               //檢查隨機生成的座位是否都是空的 
+	  	if(seats[row][col+i]!='-'){          //如果座位不是空的 
 	  		fit=0;
 	  		break;
 		  }
 	  }
       
-      if(fit){
+      if(fit){                      //如果所有座位都適合 
       	for(i=0;i<numSeats;i++){
       		seats[row][col+i]='@';
 		  }
-		  found=1;
+		  found=1;                //表示找到了適合的座位，退出迴圈 
 	  }
  
    }
 }
 
  int OKinput(char *input, int *row, int *col) {
-    if (strlen(input) != 3 || input[1] != '-') return 0;
+    if (strlen(input) != 3 || input[1] != '-') return 0;           //檢查輸入的格式是否是正確的   
 
-    *row = input[0] - '1';
-    *col = input[2] - '1';
+    *row = input[0] - '1';          //將第一個數字轉換為行 
+    *col = input[2] - '1';           //將第三個數字轉換為列 
 
-    if (*row < 0 || *row >= ROWS || *col < 0 || *col >= COLS) return 0;
+    if (*row < 0 || *row >= ROWS || *col < 0 || *col >= COLS) return 0;    //檢查行和列是否在合理範圍內 
 
     return 1;
 }
@@ -126,32 +126,32 @@ int main(void){
 	char seats[ROWS][COLS],choice,cont;
     int input=0,n=0,i=3;
    
-    printf("Please enter your password(You have 3 chance!)\n");
+    printf("Please enter your password(You have 3 chance!)\n");       
     scanf("%d",&input);
-    while(input!=2024){
-    	n++;
-    	if(n==3){
+    while(input!=2024){                   //判斷輸入的密碼是不是2024 
+    	n++;                           
+    	if(n==3){                    //當n=3時，程式結束 
 		 printf("Wrong 3 time already");
 		return 0;}
 		i--;
-    printf("Wrong Please try again(%d chance)\n",i);	 
+    printf("Wrong Please try again(%d chance)\n",i);	 //讓使用者重新輸入 
     	scanf("%d",&input);
 	}
-    printf("Correct!welcome!!\n");
+    printf("Correct!welcome!!\n");                //輸入正確，歡迎你 
     system("pause");
     system("cls");
 	
 	
 	Firstseats(seats);
 	    do{
-		
-	    	Booking_system();
-	    	choice=getchar();
-	    	clearInputBuffer() ;
+	
+	    	Booking_system();                 //顯示選單 
+	    	choice=getchar();                 //取得使用者輸入的字元 
+	    	clearInputBuffer() ;              //清空輸入緩衝區 
 	    	
 	    switch(choice){
 		
-	    	case 'a':
+	    	case 'a':                                   //顯示被訂走的位子 
 	    		printf("Available seats:\n");
 	    		displayseats(seats);
 	    	     printf("press enter to return the Booking_system...");
@@ -159,28 +159,28 @@ int main(void){
 	    	     break;
 			case 'b':{
 				
-			     int numSeats;
-			    printf("How many seats do you want(1~4) ");               
+			     int numSeats;                                
+			    printf("How many seats do you want(1~4) ");            //詢問使用者要多少個座位              
                 scanf("%d", &numSeats);
                 clearInputBuffer();
-			    if (numSeats < 1 || numSeats > 4) {
+			    if (numSeats < 1 || numSeats > 4) {            //檢查輸入是否有效 
                     printf("wrong number. press enter to return the Booking_system...");
                     getchar();
                     break;
                 }
 			    char CopySeats[ROWS][COLS];
-                   memcpy(CopySeats, seats, sizeof(seats));
-                   suggestSeats(CopySeats, numSeats);
+                   memcpy(CopySeats, seats, sizeof(seats));           //複製初始化座位表 
+                   suggestSeats(CopySeats, numSeats);           //座位建議函數，把參數丟入 
 
                 
                    printf("Suggested seats:\n");
                     displayseats(CopySeats);
 
-                    printf("Do you like (y/n)?: ");
+                    printf("Do you like (y/n)?: ");       //詢問使用者是否滿意 
                     cont = getchar(); 
                     clearInputBuffer();
 
-                if (cont=='y' || cont=='Y') {
+                if (cont=='y' || cont=='Y') {         //滿意的話，就更新座位表 
                 memcpy(seats,CopySeats,sizeof(seats));
             }
                 break;
@@ -190,21 +190,21 @@ int main(void){
 		    case 'c':{
 		    	char input[4];
 		    	int row,col;
-		    	printf("Enter the seats you want(ex: 1-2)");
+		    	printf("Enter the seats you want(ex: 1-2)");        //叫使用者輸入想坐的座位 
 		    	scanf("%s",&input);
 		    	clearInputBuffer();
-		    	if(!OKinput(input,&row,&col) || seats[row][col]!='-'){
+		    	if(!OKinput(input,&row,&col) || seats[row][col]!='-'){           //檢查輸入是否有效 
 		    		printf("wrong or the seats already taken");
 		    		getchar();
 		    		break;
 				}
-		    	seats[row][col] = '@';
+		    	seats[row][col] = '@';                 //將輸入的座位標示為@ 
                 printf("Chosen seats:\n");
                 displayseats(seats);
                 printf("press enter to return the Booking_system...");
                 getchar();
 
-                int i,j;
+                int i,j;                                //將座位標示為* 
                 for(i=0;i<ROWS;i++){
                     for(j=0;j<COLS;j++){
                         if(seats[i][j]=='@'){
@@ -217,14 +217,14 @@ int main(void){
 			}
 		    
 		    case 'd':{
-				printf("Continue? (y/n):");
-                cont = getchar(); 
+				printf("Continue? (y/n):");          //詢問是否繼續 
+                cont = getchar();   
                 clearInputBuffer();
 
                 if(cont=='n' ||cont=='N'){
-                    printf("Over!!\n");
+                    printf("Over!!\n");                     //輸入n就結束程式 
                     return 0;
-                } else if(cont=='y' || cont=='Y'){
+                } else if(cont=='y' || cont=='Y'){           //輸入y就回到選單 
                     printf("OK!press enter to return the Booking_system...");
                     getchar();
                 }
@@ -232,7 +232,7 @@ int main(void){
 				
 				break;
 			}
-		    default:
+		    default:                     //當輸入的字元不是a b c d  
                 printf("Wrong!Please enter a b c d \n");
                 printf("press enter to return the Booking_system...");
                 getchar();
@@ -240,7 +240,7 @@ int main(void){
 		    
 		    
        	}
-			}while(choice!=5);
+			}while(choice!=5);    //當choice不是5的時候，重複迴圈 
 
 		
 		
