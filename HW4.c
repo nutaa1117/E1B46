28b5397d5da6529570d471d83482565070f28864
施家誠 <u11202246@o365.ttu.edu.tw>
2024-06-01 15:55:14 +0800

    輸入學生姓名 用結構陣列存取 所輸入的資料，後面的 寫法都一樣，我就寫完再 貼上來。

--- HW4.c
+++ HW4.c
@@ -30,10 +30,10 @@ struct Student{
 	int math;
 	int physics;
 	int english;
-	float averge;
+	float average;
 	
 };
-Student students[10];
+struct Student students[10];
 int a=0;
 
 
@@ -49,6 +49,31 @@ void cleantoo(){
 void studentlist(){
 	clean();
 	printf("請輸入要填的學生人數(5~10)");
+	int b;
+	scanf("%d",&b);
+	while(b<5 || b>10){
+		printf("輸入不在範圍內，請重新嘗試:");
+		scanf("%d",&b);
+		cleantoo();
+	}
+	a=b;
+	int i;
+	for( i=0;i<b;i++){
+		printf("輸入學生姓名:");
+		scanf("%s",students[i].name);
+		printf("輸入學號 (6位整數):");
+		scanf("%d",&students[i].id);
+		while(students[i].id<100000 || students[i].id>999999){
+			printf("輸入錯誤，請重新輸入:");
+		    scanf("%d",&students[i].id);
+		    cleantoo();
+		}
+		
+		
+		
+	}
+	
+	
 }
 
 
