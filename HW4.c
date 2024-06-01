634dfae690a97ba3e6ab74d2756ffc66dce53a6b
施家誠 <u11202246@o365.ttu.edu.tw>
2024-06-01 12:40:23 +0800

    個人風格 這次個人風格畫面我想要做我的 座號，46號的一個畫面。

---
+++ HW4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+void mynumber(){
+	printf("¡X--------------------------------------------------------------------------------------------\n");
+	printf("!                          44    44                                        66                              !\n");
+	printf("!                        44      44                                   66                                   !\n");
+	printf("!                      44        44                              66                                        !\n");
+	printf("!                    44          44                           66                                           !\n");
+	printf("!                  44            44                         66                                             !\n");
+	printf("!                44              44                       66                                               !\n");
+	printf("!              44                44                      66               66                             !\n");
+	printf("!            44                  44                     66      66                  66                 !\n");
+	printf("!          444444444444444444444      66  66                           66             !\n");
+	printf("!          444444444444444444444     66                                      66         !\n");
+	printf("!                                 44                    66                                        66       !\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+}
+
+
+
+
+
+
+
+void clean() {
+    system("cls"); 
+}
+
+
+
