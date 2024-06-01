e2c8269400590334d6e7f16366b7de52eb56acaf
施家誠 <u11202246@o365.ttu.edu.tw>
2024-06-01 13:15:45 +0800

    完成風格圖+做第三題 新增了一個結構student， 裡面包含了名字、id、 數學成績、物理、英文 和平均，然後還有一個結構陣列 ，最多可以填入10人的資料， 然後還有一個函數cleantoo， 是拿來清除緩衝區的，避免讀取到 別的字符，導致程式卡住。

--- HW4.c
+++ HW4.c
@@ -6,32 +6,55 @@
 
 void mynumber(){
 	printf("—--------------------------------------------------------------------------------------------\n");
-	printf("!                          44    44                                        66                              !\n");
-	printf("!                        44      44                                   66                                   !\n");
-	printf("!                      44        44                              66                                        !\n");
-	printf("!                    44          44                           66                                           !\n");
-	printf("!                  44            44                         66                                             !\n");
-	printf("!                44              44                       66                                               !\n");
-	printf("!              44                44                      66               66                             !\n");
-	printf("!            44                  44                     66      66                  66                 !\n");
-	printf("!          444444444444444444444      66  66                           66             !\n");
-	printf("!          444444444444444444444     66                                      66         !\n");
-	printf("!                                 44                    66                                        66       !\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
+	printf("!                       44  44                                 66                            !\n");
+	printf("!                      44   44                               66                              !\n");
+	printf("!                     44    44                            66                                 !\n");
+	printf("!                    44     44                          66                                   !\n");
+	printf("!                   44      44                         66                                    !\n");
+	printf("!                  44       44                        66                                     !\n");
+	printf("!                 44        44                       66           66                         !\n");
+	printf("!                44         44                       66      66        66                    !\n");
+	printf("!               444444444444444444444                66  66             66                   !\n");
+	printf("!               444444444444444444444                66                  66                  !\n");
+	printf("!                           44                       66                   66                 !\n");
+	printf("!                           44                        66                  66                 !\n");
+	printf("!                           44                         66                 66                 !\n");
+	printf("!                           44                           66             66                   !\n");
+	printf("!                           44                             66   66   66                      !\n");
+	printf("—--------------------------------------------------------------------------------------------\n");
 }
 
-
-
-
-
+struct Student{
+	char name[50];
+	int id;
+	int math;
+	int physics;
+	int english;
+	float averge;
+	
+};
+Student students[10];
+int a=0;
 
 
 void clean() {
     system("cls"); 
 }
 
+void cleantoo(){
+	int c;
+	while((c=getchar())!='\n' && c!=EOF);
+}
 
+void studentlist(){
+	clean();
+	printf("請輸入要填的學生人數(5~10)");
+}
+
+
+int main(void){
+	mynumber();
+	system("pause");
+	clean();
+}
 
